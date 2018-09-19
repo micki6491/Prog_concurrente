1572bb0da5eea849d07bd275742110c1ba019ac9
Kneuss Michaël <michael.kneuss@he-arc.ch>
2018-09-19 21:10:56 +0200

    Ex 1 et 2 série 1

---
+++ serie01_ex1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(){
+
+    int size = 7;
+    int tab[] = {0,6,3,5,2,4,1};
+    printf("Unsorted tab :\n");
+    printTab(tab,size);
+    mySort(tab, size);
+    printf("Sorted tab :\n");
+    printTab(tab,size);
+    return 0;
+}
+
+void printTab(int* tab, int size)
+{
+    int i = 0;
+    for(i; i<size;i++)
+    {
+        if(i <size-1)
+            printf("%d, ", tab[i]);
+        else
+            printf("%d ", tab[i]);
+    }
+    printf("\n");
+}
+
+void mySort(int* tab, int size)
+{
+    int i = 0;
+    int n = 0;
+    for(i; i < size; i++)
+    {
+        if(n < size)
+        {
+            int a = getSmallerElement(tab,size,n);
+            switchElement(a, n, tab);
+            n+=1;
+        }
+    }
+}
+void switchElement(int a, int b, int* tab)
+{
+    int temp = tab[a];
+    tab[a] = tab[b];
+    tab[b] = temp;
+}
+
+int getSmallerElement(int* tab, int size, int n)
+{
+    int i = 0+n;
+    int temp = tab[i];
+    for(i; i<size;i++)
+        if(temp > tab[i])
+            temp = i;;
+    return temp;
+}
+
